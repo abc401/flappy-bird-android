6a6407cd5ea0dada9bd3511ebbf922dacaab119d
koibhinheen <koibhinheen@gmail.com>
2023-04-06 20:03:19 +0500

    GamePlay activity

--- app/src/main/java/com/example/flappybird/MainActivity.kt
+++ app/src/main/java/com/example/flappybird/MainActivity.kt
@@ -1,19 +1,18 @@
 package com.example.flappybird
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.Button
 
-class MainMenu : AppCompatActivity() {
-
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        setContentView(R.layout.activity_main_menu)
+        setContentView(R.layout.activity_main)
     }
 
     fun playGame(view: View) {
-
+        val intent = Intent(applicationContext, GamePlay::class.java)
+        startActivity(intent)
     }
 }
