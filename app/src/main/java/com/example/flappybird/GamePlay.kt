834c54325795c35408447c6fe02557d8e147372b
koibhinheen <koibhinheen@gmail.com>
2023-04-06 19:15:09 +0500

    Initial Commit

---
+++ app/src/main/java/com/example/flappybird/GamePlay.kt
@@ -0,0 +1,11 @@
+package com.example.flappybird
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class GamePlay : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_game_play)
+    }
+}
---
+++ app/src/main/java/com/example/flappybird/MainMenu.kt
@@ -0,0 +1,19 @@
+package com.example.flappybird
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+
+class MainMenu : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_main_menu)
+    }
+
+    fun playGame(view: View) {
+
+    }
+}
